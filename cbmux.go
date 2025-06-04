084af5950406115d341a32e78b9b42109724cee1
M <m@cognusion.com>
2025-06-04 19:33:54 -0400

    Relocated Settings{} and dumped a bunch of docs, mostly from gobreaker.

--- cbmux.go
+++ cbmux.go
@@ -13,13 +13,6 @@ import (
 	"github.com/sony/gobreaker/v2"
 )
 
-// Settings allows for per-mux and per-'breaker configurations.
-type Settings[T any] struct {
-	gobreaker.Settings
-	ExecClosure ExecFunc[T]
-	ExpireAfter time.Duration
-}
-
 // CircuitBreakerMux is a goro-safe circuit breaker multiplex,
 // whereby individual keys gets their own 'breakers,
 // which can each be in various states.
@@ -76,6 +69,45 @@ func (c *CircuitBreakerMux[T]) Clear() {
 
 // ExecFunc is a closure to allow a string to be passed to an otherwise niladic function.
 type ExecFunc[T any] func(string) func() (T, error)
+
+// Settings allows for per-mux and per-'breaker configurations.
+//
+// Name is the name of the CircuitBreaker. This is overridden per-'breaker.
+//
+// MaxRequests is the maximum number of requests allowed to pass through
+// when the CircuitBreaker is half-open.
+// If MaxRequests is 0, the CircuitBreaker allows only 1 request.
+//
+// Interval is the cyclic period of the closed state
+// for the CircuitBreaker to clear the internal Counts.
+// If Interval is less than or equal to 0, the CircuitBreaker doesn't clear internal Counts during the closed state.
+//
+// Timeout is the period of the open state,
+// after which the state of the CircuitBreaker becomes half-open.
+// If Timeout is less than or equal to 0, the timeout value of the CircuitBreaker is set to 60 seconds.
+//
+// ReadyToTrip is called with a copy of Counts whenever a request fails in the closed state.
+// If ReadyToTrip returns true, the CircuitBreaker will be placed into the open state.
+// If ReadyToTrip is nil, default ReadyToTrip is used.
+// Default ReadyToTrip returns true when the number of consecutive failures is more than 5.
+//
+// OnStateChange is called whenever the state of the CircuitBreaker changes.
+//
+// IsSuccessful is called with the error returned from a request.
+// If IsSuccessful returns true, the error is counted as a success.
+// Otherwise the error is counted as a failure.
+// If IsSuccessful is nil, default IsSuccessful is used, which returns false for all non-nil errors.
+//
+// ExecClosure is a closure to allow a string to be passed to an otherwise niladic function for execution by
+// the 'breakers.
+//
+// ExpireAfter is a Duration after which an unused 'breaker may be removed for the mux.
+// If ExpireAfter is less than or equal to 0, expiration will not occur.
+type Settings[T any] struct {
+	gobreaker.Settings
+	ExecClosure func(string) func() (T, error)
+	ExpireAfter time.Duration
+}
 
 type cache struct {
 	item  any
